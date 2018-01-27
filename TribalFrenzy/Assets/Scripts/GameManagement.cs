ffa0276537f6b80d96b4b30736c0b67e0c2bc4ef
Daniel Maida <dfm2@cin.ufpe.br>
2018-01-27 04:59:43 -0300

    cutscene management start added

---
+++ TribalFrenzy/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour {
+
+    public int numberOfLives;
+    public GameObject[] LifeIcons;
+
+    public void Start()
+    {
+        if (PlayerPrefs.GetInt("Level") == 0) {
+            PlayerPrefs.SetInt("Level", 1);
+        } 
+    }
+
+    public void DecreaseLife() {
+        numberOfLives--;
+        Destroy(LifeIcons[numberOfLives]);
+        if(numberOfLives <= 0)
+            SceneManager.LoadScene("main_menu");
+    }
+
+    public void CompleteLevel() {
+        int level = PlayerPrefs.GetInt("Level") + 1;
+        PlayerPrefs.SetInt("Level", level);
+        SceneManager.LoadScene("cutscene_" + level); /*colocar aqui a transicao da cutscene.*/
+    }
+}

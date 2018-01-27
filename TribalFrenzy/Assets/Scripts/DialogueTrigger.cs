0bf3f5141609d1bc1963a0292fd3e8ba0a76e1c2
Daniel Maida <dfm2@cin.ufpe.br>
2018-01-27 18:50:13 -0300

    cutscene ready

---
+++ TribalFrenzy/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+    public Dialogue dialogue;
+    private bool started;
+
+    public void Start()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.Return) )
+            TriggerDialogue();
+        }
+
+    public void TriggerDialogue() {
+            FindObjectOfType<DialogueManager>().DisplayNextSentence();
+    }
+}
---
+++ TribalFrenzy/Assets/Scripts/TextBlink.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBlink : MonoBehaviour {
+
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        StartCoroutine("Blink");
+	}
+
+    IEnumerator Blink() {
+        while (true)
+        {
+            text.text = "";
+            yield return new WaitForSeconds(.5f);
+            text.text = ". . .";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+	
+}
